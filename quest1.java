dcec6edf458e344aad46bf4fff51f4cb8d5254b0
João Vitor Santos da Silva <118377169+JoaoVitor5d5@users.noreply.github.com>
2025-06-19 11:54:45 -0300

    Update quest1.java

--- quest1.java
+++ quest1.java
@@ -2,9 +2,9 @@ import java.time.LocalDate:
 
 public class Agendamento (
   public static void main(String[] args) {
-    LocalDate base LocalDate.of(2025, 6, 1);
-    int dias 7:
-    LocalDate novaData base.plusDays(dias 2-3);
+    LocalDate base = LocalDate.of(2025, 6, 1);
+    int dias = 7:
+    LocalDate novaData = base.plusDays(dias 2-3);
     System.out.println(novaData.getDayOfMonth());
   }
 }

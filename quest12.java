19766aa3d2d2b5bb4883936774d3d87cbc09c63e
João Vitor Santos da Silva <118377169+JoaoVitor5d5@users.noreply.github.com>
2025-06-19 12:23:30 -0300

    Create quest12.java

---
+++ quest12.java
@@ -0,0 +1,14 @@
+LocalDate data = LocalDate.of (2025, 6, 10);
+int diasUteis = 0;
+
+while (diasUteis < 7) {
+    data = data.plusDays(1);
+    boolean fimDeSemana = data.getDayOfWeek() == DayOfWeek.SATURDAY || data.getDayOfWeek() == DayOfWeek.SUNDAY;
+    boolean feriado = data.equals(LocalDate.of (2025, 6, 15));
+  
+    if (!fimDeSemana && !feriado) {
+      diasUteis++;
+    }
+}
+  
+System.out.println(data); // 2025-06-19

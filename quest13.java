1d5d68ac539695f2de2b6e9d1f60ececb0820e95
João Vitor Santos da Silva <118377169+JoaoVitor5d5@users.noreply.github.com>
2025-06-19 12:26:40 -0300

    Create quest13.java

---
+++ quest13.java
@@ -0,0 +1,7 @@
+LocalTime envio = LocalTime.of(23, 0);
+LocalTime limite = LocalTime.of(22, 59);
+
+if (!envio.isBefore(limite)) {
+  System.out.println("Entrega fora do horário.");
+} else {
+  System.out.println("Tarefa enviada com sucesso.");
